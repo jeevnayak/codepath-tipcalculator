4f69030550c34aa88b73d4516ba151a470e4f3ea
Rajeev Nayak <jeev@Rajeevs-MacBook-Pro.local>
2014-05-07 22:42:30 -0700

    Initial Commit

---
+++ tipcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by Rajeev Nayak on 5/7/14.
+//  Copyright (c) 2014 jeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
